7f6aae5d4f626bedeb6ccc113c412b746ab116b8
logikgate <nick.fredricks@flypaper.com>
2018-10-25 14:10:29 -0400

    Make travis use postgresql

--- test/dbscribe.spec.ts
+++ test/dbscribe.spec.ts
@@ -1,4 +1,5 @@
 process.env.SCRIBE_APP_DB_NAME = "test"
+process.env.SCRIBE_APP_DB_USER = "postgres"
 import {createServer} from "../src/scribe.cli"
 import * as mocha from "mocha"
 import * as chai from "chai"
