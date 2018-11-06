ed1720ee6eeb7f54c2c3272b379cb6ff7e72f4a2
logikgate <nick.fredricks@flypaper.com>
2018-11-05 22:57:49 -0500

    Remove dist from repo, make deploy build to dist for package

--- dist/scribe.cli.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});const cluster=require("cluster");const os=require("os");const scribe_1=require("./scribe");if(cluster.isMaster){let cores=os.cpus();for(let i=0;i<cores.length;i++){cluster.fork()}cluster.on("exit",worker=>{cluster.fork()})}else{scribe_1.createServer()}
